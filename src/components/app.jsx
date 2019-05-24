97aa4ba9979d9c6659931f98b6a1e7b0f0ccded6
jacob coccari <jacob.coccari@gmail.com>
2019-05-24 08:46:39 -0700

    got the map to render, finally

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
 
 import FlatList from './flat_list.jsx';
 
@@ -13,15 +14,28 @@ class App extends Component {
   }
 
   search = (lat, lng) => {
-    //Insert google API call here !!
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '1000px', width: '1000px', position: 'absolute' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={{lat: 59.95, lng: 30.33}}
+          defaultZoom={12}
+        >
+        </GoogleMapReact>
+      </div>
+    );
   }
 
   render() {
     return (
 
         <div>
-          <div className="flats">
+          <div className="flat-list">
             <FlatList flats={this.flats} />
+          </div>
+          <div className="map-container">
+            {this.search(10, 10)}
           </div>
         </div>
     );
