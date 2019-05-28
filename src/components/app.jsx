e13637f22a432300e8afa34f70c3f5c357b6a322
jacob coccari <jacob.coccari@gmail.com>
2019-05-28 15:48:46 -0700

    added the marker onto the map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
 
 import FlatList from './flat_list.jsx';
+import Marker from './marker.jsx';
+
 
 class App extends Component {
   constructor(props) {
@@ -31,6 +33,7 @@ class App extends Component {
           defaultCenter={{lat: this.state.lat, lng: this.state.lng}}
           defaultZoom={12}
         >
+        <Marker lat={this.state.lat} lng={this.state.lng} />
         </GoogleMapReact>
       </div>
     );
